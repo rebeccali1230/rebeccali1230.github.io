ad8b46d2ee64816261775552bdc15e6a599d2d9d
RebeccaLi <li000869@algonquinlive.com>
2023-09-22 15:24:59 -0400

    Completed Lab 2

---
+++ lab-2-alt/script.js
@@ -0,0 +1,16 @@
+// declare variable
+const firstName = 'Rebecca'
+
+// concatenate variable
+console.log(`My name is ${firstName}`)
+
+// declare variable 
+const favNumber = 8
+
+// declare variable
+const squared= (favNumber * favNumber)
+
+// display message to console
+// testing
+// test
+console.log(`The squared of ${favNumber} is ${squared}`)
