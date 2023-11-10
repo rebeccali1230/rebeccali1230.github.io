113578a83f6842da45fa5ccf185a8a37f7b71fdc
RebeccaLi <li000869@algonquinlive.com>
2023-11-09 22:32:26 -0500

    Complete Lab 7

--- lab-7/script.js
+++ lab-7/script.js
@@ -1,59 +1,61 @@
 /* Create a function that retrieves every type of bean from the counters data, stores it into an array, and returns the array. */
 /*The array should only contain unique values. */
 
-function getBeanTypes() {
-    let beanTypes = [];
-    counters.forEach(counter => {
-        if (!beanTypes.includes(counter.bean)) {
-            beanTypes.push(counter.bean);
+function getUniqueBeans(countersArray) {
+    const uniqueBeans = [];
+    countersArray.forEach(counter => {
+        if (!uniqueBeans.includes(counter.bean)) {
+            uniqueBeans.push(counter.bean);
         }
     });
-    return beanTypes;
+    return uniqueBeans;
 }
 
 /* Create a function that sorts the counters data by the counters' names and returns the sorted array. */
 
-function sortCountersByName() {
-    return counters.sort((a, b) => a.name.localeCompare(b.name));
+function sortCountersByName(countersArray) {
+    return countersArray.slice().sort((a, b) => a.name.localeCompare(b.name));
 }
 
 /* Create a function that filters the counters by bean type and returns the filtered array. */
 
-function filterCountersByBean(bean) {
-    return counters.filter(counter => counter.bean === bean);
+function filterCountersByBean(countersArray, beanType) {
+    return countersArray.filter(counter => counter.bean === beanType);
 }
 
 /* Create a function that returns the total number of beans counted by every counter in the provided counters array. Return the total number. */
 
-function getTotalCount() {
-    let totalCount = 0;
-    counters.forEach(counter => {
-        totalCount += counter.count;
-    });
-    return totalCount;
+function getTotalBeans(countersArray) {
+    return countersArray.reduce((total, counter) => total + counter.count, 0);
 }
 
 /* Dynamically create an ordered list for every bean type containing each bean counter. 
 In addition to the counter's name, the counter's count should be displayed. 
 The total number of beans for each bean counted should also be displayed. */
 
-function createBeanList() {
-    let beanTypes = getBeanTypes();
-    let beanList = document.getElementById("beanList");
-    beanTypes.forEach(bean => {
-        let beanListItem = document.createElement("li");
-        beanListItem.textContent = bean;
-        beanListItem.id = bean;
-        beanList.appendChild(beanListItem);
-        let beanCounters = filterCountersByBean(bean);
-        beanCounters.forEach(counter => {
-            let counterListItem = document.createElement("li");
-            counterListItem.textContent = counter.name + ": " + counter.count;
-            beanListItem.appendChild(counterListItem);
+function createBeanCountersList(countersArray) {
+    const uniqueBeans = getUniqueBeans(countersArray);
+    const beanListContainer = document.getElementById('beanCounterList');
+
+    uniqueBeans.forEach(bean => {
+        const beanCounters = filterCountersByBean(countersArray, bean);
+        const sortedBeanCounters = sortCountersByName(beanCounters);
+
+        const totalBeans = getTotalBeans(beanCounters);
+        const listTitle = document.createElement('h2');
+        listTitle.textContent = `${bean} - Total Count: ${totalBeans}`;
+        beanListContainer.appendChild(listTitle);
+
+        const list = document.createElement('ol');
+        sortedBeanCounters.forEach(counter => {
+            const listItem = document.createElement('li');
+            listItem.textContent = `${counter.name} - ${counter.count}`;
+            list.appendChild(listItem);
         });
-        let totalCount = getTotalCount();
-        let totalListItem = document.createElement("li");
-        totalListItem.textContent = "Total: " + totalCount;
-        beanListItem.appendChild(totalListItem);
+
+        beanListContainer.appendChild(list);
     });
 }
+
+// Call the function to create and append the list after the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', () => createBeanCountersList(counters));
